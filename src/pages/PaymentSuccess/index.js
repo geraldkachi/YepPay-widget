d022052b6cc70a1dad500bd7c50ae6848a90b343
wiztemple <wiztemple7@gmail.com>
2021-11-28 10:14:09 +0100

    fix conflicts

--- src/pages/PaymentSuccess/index.js
+++ src/pages/PaymentSuccess/index.js
@@ -8,10 +8,6 @@ const PaymentSuccess = () => {
       <div className="paymentstatus mt-50">
         <div className="w-full flex justify-center">
           <div className="icon-wrapper">
-<<<<<<< HEAD
-=======
-            {/* <img src={SuccessIcon} alt="Payment Success Icon" /> */}
->>>>>>> 2e651d95abdb4d52ecd8745aa8027a0fd3b9879c
             <AnimatedSuccessCheckmark />
           </div>
         </div>
@@ -19,7 +15,6 @@ const PaymentSuccess = () => {
           <p>Payment Successful</p>
           <span>You have successfully completed the payment of</span>
           <h1>NGN 1,000.00</h1>
-<<<<<<< HEAD
           <div className="centralize">
             <button type="button" to="/authorize_transaction" className="btn success w-200">
               <span>View Receipt</span>
@@ -30,14 +25,6 @@ const PaymentSuccess = () => {
               <span>Dismiss</span>
             </button>
           </div>
-=======
-          <button type="button" to="/authorize_transaction" className="btn block-btn success">
-            <span>View Receipt</span>
-          </button>
-          <button type="button" to="/authorize_transaction" className="btn block-btn default mt-5 mb-15">
-            <span>Dismiss</span>
-          </button>
->>>>>>> 2e651d95abdb4d52ecd8745aa8027a0fd3b9879c
         </div>
         <div className="paymentstatus-footer">
           <p>Powered by Cashenvoy</p>
