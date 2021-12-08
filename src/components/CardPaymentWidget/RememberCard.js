48528bfde73977d975509d78b3e10411fd776ec2
wiztemple <wiztemple7@gmail.com>
2021-12-08 10:46:56 +0100

    minor changes

--- src/components/CardPaymentWidget/RememberCard.js
+++ src/components/CardPaymentWidget/RememberCard.js
@@ -11,51 +11,51 @@ const RememberCard = () => {
   return (
     <>
       <div className="remembercard-collections">
-        <Link to="/existing_card" className="remembercard" type="button">
+        <div className="remembercard">
           <button type="button" onClick={() => setShowDeleteCardModal(true)}>
             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" clipRule="evenodd" d="M6.00008 7.41417L1.70718 11.7071L0.292969 10.2928L4.58586 5.99995L0.292969 1.70706L1.70718 0.292847L6.00008 4.58574L10.293 0.292847L11.7072 1.70706L7.41429 5.99995L11.7072 10.2928L10.293 11.7071L6.00008 7.41417Z" fill="#91979F" />
             </svg>
           </button>
-          <span>8940</span>
+          <Link to="/existing_card">9640</Link>
           <img src={MasterCardSmall} alt="Debit Card Icon" />
-        </Link>
-        <Link to="/existing_card" className="remembercard" type="button">
+        </div>
+        <div className="remembercard" type="button">
           <button type="button" onClick={() => setShowDeleteCardModal(true)}>
             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" clipRule="evenodd" d="M6.00008 7.41417L1.70718 11.7071L0.292969 10.2928L4.58586 5.99995L0.292969 1.70706L1.70718 0.292847L6.00008 4.58574L10.293 0.292847L11.7072 1.70706L7.41429 5.99995L11.7072 10.2928L10.293 11.7071L6.00008 7.41417Z" fill="#91979F" />
             </svg>
           </button>
-          <span>8940</span>
+          <Link to="/existing_card">8940</Link>
           <img src={MasterCardSmall} alt="Debit Card Icon" />
-        </Link>
-        <Link to="/existing_card" type="button" className="remembercard">
+        </div>
+        <div className="remembercard">
           <button type="button" onClick={() => setShowDeleteCardModal(true)}>
             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" clipRule="evenodd" d="M6.00008 7.41417L1.70718 11.7071L0.292969 10.2928L4.58586 5.99995L0.292969 1.70706L1.70718 0.292847L6.00008 4.58574L10.293 0.292847L11.7072 1.70706L7.41429 5.99995L11.7072 10.2928L10.293 11.7071L6.00008 7.41417Z" fill="#91979F" />
             </svg>
           </button>
-          <span>8056</span>
+          <Link to="/existing_card">0940</Link>
           <img src={VisaCardSm} alt="Debit Card Icon" />
-        </Link>
-        <Link to="/existing_card" type="button" className="remembercard">
+        </div>
+        <div className="remembercard">
           <button type="button" onClick={() => setShowDeleteCardModal(true)}>
             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" clipRule="evenodd" d="M6.00008 7.41417L1.70718 11.7071L0.292969 10.2928L4.58586 5.99995L0.292969 1.70706L1.70718 0.292847L6.00008 4.58574L10.293 0.292847L11.7072 1.70706L7.41429 5.99995L11.7072 10.2928L10.293 11.7071L6.00008 7.41417Z" fill="#91979F" />
             </svg>
           </button>
-          <span>8056</span>
+          <Link to="/existing_card">8056</Link>
           <img src={VisaCardSm} alt="Debit Card Icon" />
-        </Link>
-        <Link to="/existing_card" type="button" className="remembercard">
+        </div>
+        <div className="remembercard">
           <button type="button" onClick={() => setShowDeleteCardModal(true)}>
             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" clipRule="evenodd" d="M6.00008 7.41417L1.70718 11.7071L0.292969 10.2928L4.58586 5.99995L0.292969 1.70706L1.70718 0.292847L6.00008 4.58574L10.293 0.292847L11.7072 1.70706L7.41429 5.99995L11.7072 10.2928L10.293 11.7071L6.00008 7.41417Z" fill="#91979F" />
             </svg>
           </button>
-          <span>8056</span>
+          <Link to="/existing_card">0406</Link>
           <img src={VisaCardSm} alt="Debit Card Icon" />
-        </Link>
+        </div>
       </div>
       {
         showDeleteCardModal && (<DeleteCardModal setShowDeleteCardModal={setShowDeleteCardModal} />)
