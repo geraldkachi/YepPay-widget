b5e8a4b60c741cd00079d9af0d6537bc2a06393e
Sodiq Alase <sodiqalase@yahoo.com>
2023-01-13 10:46:29 +0100

    removed hardcoded values

--- src/components/Redirect/index.js
+++ src/components/Redirect/index.js
@@ -4,7 +4,7 @@ import { availablePaymentChannels } from "../WidgetHeader/index";
 
 const urls = {
 	card: "card",
-	// ussd: "ussd-payment",
+	ussd: "ussd-payment",
 	// bank: "bank-transfer",
 	"offline transfer": "offline-transfer",
 };
@@ -18,13 +18,11 @@ const RedirectToFirstChannel = ({ paymentDetail }) => {
 		availablePaymentChannels.includes(channel)
 	);
 
-
-
 	if (channelsAvailable.length === 0) {
 		return <Redirect to={`/error/${accessCode}`} />;
 	}
 
 	return <Redirect to={`/${accessCode}/${urls[channelsAvailable[0]]}`} />;
-};;
+}
 
 export default RedirectToFirstChannel;
--- src/pages/RedirectWidget/index.js
+++ src/pages/RedirectWidget/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import withInitiatePayment from "../../components/HOC/withInitiatePayment";
-import RedirectToCard from "../../components/Redirect";
+import RedirectToFirstChannel from "../../components/Redirect";
 
 const RedirectWidget = ({ paymentDetail }) => {
-	return <RedirectToCard paymentDetail={paymentDetail} />;
+	return <RedirectToFirstChannel paymentDetail={paymentDetail} />;
 };
 
 export default withInitiatePayment(RedirectWidget);
